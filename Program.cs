ef5eb59ed4f55fd5ab18a98c09b5fe27d9d10ce0
David Rolland <rold2007@yahoo.com>
2026-03-05 20:24:38 +0900

    Started project.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Spectre.Console;
+
+var canvas = new Canvas(40, 20);
+
+canvas.Scale = false;
+
+AnsiConsole.Live(canvas)
+    .Start(ctx =>
+    {
+        int count = 0;
+
+        while (true)
+        {
+            // Fill background
+            for (var y = 0; y < 20; y++)
+            {
+                for (var x = 0; x < 40; x++)
+                {
+                    // canvas.SetPixel(x, y, Color.Grey11);
+                    if (count % 2 == 0)
+                        canvas.SetPixel(x, y, Color.Red);
+                    else
+                        canvas.SetPixel(x, y, Color.Yellow);
+                }
+            }
+
+            ctx.Refresh();
+            Thread.Sleep(1000);
+            count++;
+        }
+    });
